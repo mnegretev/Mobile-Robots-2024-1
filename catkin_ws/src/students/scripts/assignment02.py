6c63edc84b61bc117157f25160fd5fdb1a9dfb41
Erik Yoel Santana Apreza <54052333+eysa16@users.noreply.github.com>
2023-08-28 16:36:00 -0600

    Update assignment02.py
    Se anexaron los cambios que permiten a Justina la detección de obstáculos a una determinada distancia.

--- catkin_ws/src/students/scripts/assignment02.py
+++ catkin_ws/src/students/scripts/assignment02.py
@@ -17,11 +17,8 @@ NAME = "FULL NAME"
 
 def callback_scan(msg):
     global obstacle_detected
-    #
-    # TODO:
-    # Do something to detect if there is an obstacle in front of the robot.
-    # Set the 'obstacle_detected' variable with True or False, accordingly.
-    #
+    n = int((msg.angle_max - msg.angle_min)/msg.angle_increment/2)
+    obstacle_detected = msg.ranges[n] < 1.0
     
     return
 
@@ -35,13 +32,9 @@ def main():
     global obstacle_detected
     obstacle_detected = False
     while not rospy.is_shutdown():
-        #
-        # TODO:
-        # Declare a Twist message and assign the appropiate speeds:
-        # Move forward if there is no obstacle in front of the robot, and stop otherwise.
-        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
-        # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        #
+        msg_cmd_vel = Twist()
+        msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
+        pub_cmd_vel.publish(msg_cmd_vel)
         
         loop.sleep()
 
