e4153b31db127d060795420290d5e053090f7807
Erickvilledah <erickricardovilledahdz@gmail.com>
2023-09-03 15:32:10 -0500

    Practica 1.

--- catkin_ws/src/students/scripts/practice01a.py
+++ catkin_ws/src/students/scripts/practice01a.py
@@ -15,7 +15,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "Villeda Hernandez Erick Ricardo"
+NAME = "Villeda Hernandez Erick Ricardo."
 
 def get_inflated_map(static_map, inflation_cells):
     print("Inflating map by " + str(inflation_cells) + " cells")
--- catkin_ws/src/students/scripts/practice01c.py
+++ catkin_ws/src/students/scripts/practice01c.py
@@ -19,7 +19,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL_NAME"
+NAME = "Villeda Hernandez Erick Ricardo."
 
 def brushfire(grid_map):
     print("Executing brushfire algorithm...")
