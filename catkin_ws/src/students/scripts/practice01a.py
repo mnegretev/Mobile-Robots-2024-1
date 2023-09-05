e43d5f58ecf9dc0f5d1063ee41b80ac0400ebe37
WichoPrest32 <nanorocko@hotmail.com>
2023-09-04 22:11:06 -0500

    Practica 1

--- catkin_ws/src/students/scripts/practice01a.py
+++ catkin_ws/src/students/scripts/practice01a.py
@@ -9,26 +9,35 @@
 #
 
 import rospy
-import numpy
+import numpy as np
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
 NAME = "FULL_NAME"
+inflated_map = None
 
 def get_inflated_map(static_map, inflation_cells):
     print("Inflating map by " + str(inflation_cells) + " cells")
-    inflated = numpy.copy(static_map)
+    inflated = np.copy(static_map)
     [height, width] = static_map.shape
-    #
-    # TODO:
-    # Write the code necessary to inflate the obstacles in the map a radius
-    # given by 'inflation_cells' (expressed in number of cells)
-    # Map is given in 'static_map' as a bidimensional numpy array.
-    # Consider as occupied cells all cells with an occupation value greater than 50
-    #
-        
+
+    # Itera a través de cada celda del mapa estático
+    for i in range(height):
+        for j in range(width):
+            # Considerar celdas con valor de ocupación superior a 50 como obstáculos
+            if static_map[i, j] > 50:  
+                # Itera sobre las celdas circundantes para inflar
+                for dx in range(-inflation_cells, inflation_cells + 1):
+                    for dy in range(-inflation_cells, inflation_cells + 1):
+                        x = i + dx
+                        y = j + dy
+                        # Verifica si las coordenadas están dentro de los límites del mapa
+                        if 0 <= x < height and 0 <= y < width:
+                            # Establece la celda correspondiente en el mapa inflado como obstáculo
+                            inflated[x, y] = 100  #  Ajustar el valor necesario
+
     return inflated
 
 def callback_inflated_map(req):
@@ -36,7 +45,7 @@ def callback_inflated_map(req):
     return GetMapResponse(map=inflated_map)
 
 def main():
-    global cost_map, inflated_map
+    global inflated_map
     print("PRACTICE 01 A - Map Inflation - " + NAME)
     rospy.init_node("practice01a")
     rospy.wait_for_service('/static_map')
@@ -44,7 +53,7 @@ def main():
     grid_map = rospy.ServiceProxy("/static_map", GetMap)().map
     map_info = grid_map.info
     width, height, res = map_info.width, map_info.height, map_info.resolution
-    grid_map = numpy.reshape(numpy.asarray(grid_map.data, dtype='int'), (height, width))
+    grid_map = np.reshape(np.asarray(grid_map.data, dtype='int'), (height, width))
     rospy.Service('/inflated_map', GetMap, callback_inflated_map)
     loop = rospy.Rate(1)
     
@@ -53,7 +62,7 @@ def main():
         inflation_radius = rospy.get_param("~inflation_radius")
     while not rospy.is_shutdown():
         inflated_map_data = get_inflated_map(grid_map, round(inflation_radius/res))
-        inflated_map_data = numpy.ravel(numpy.reshape(inflated_map_data, (width*height, 1)))
+        inflated_map_data = np.ravel(np.reshape(inflated_map_data, (width*height, 1)))
         inflated_map      = OccupancyGrid(info=map_info, data=inflated_map_data)
         pub_map.publish(callback_inflated_map(GetMapRequest()).map)
         loop.sleep()
@@ -63,4 +72,5 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
+
     
