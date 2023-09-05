1de80a97d6ff77d26ecbcb1772893cd3f62537f6
BranMM <78185047+BranMM@users.noreply.github.com>
2023-09-05 14:57:37 -0600

    Update assignment02.py

--- catkin_ws/src/students/scripts/assignment02.py
+++ catkin_ws/src/students/scripts/assignment02.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "FULL NAME"
+NAME = "Brandon Meza Martínez"
 
 def callback_scan(msg):
     global obstacle_detected
