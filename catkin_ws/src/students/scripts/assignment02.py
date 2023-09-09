681456de0fa17afbc8a84685e925d0f3de05dbdb
Eduardo Flores Rivas <eduardoflorivas@gmail.com>
2023-09-09 07:34:21 -0600

    Se cambió la variable FULL NAME por mi nombre completo en los archivos assignment02.py, practice01a.py, practice01b.py y practice01c.py

--- catkin_ws/src/students/scripts/assignment02.py
+++ catkin_ws/src/students/scripts/assignment02.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "FULL NAME"
+NAME = "Eduardo Flores Rivas"
 
 def callback_scan(msg):
     global obstacle_detected
--- catkin_ws/src/students/scripts/practice01c.py
+++ catkin_ws/src/students/scripts/practice01c.py
@@ -19,7 +19,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL_NAME"
+NAME = "Eduardo Flores Rivas"
 
 def brushfire(grid_map):
     print("Executing brushfire algorithm...")
