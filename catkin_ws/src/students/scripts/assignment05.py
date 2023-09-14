d9bdbf3be181f99b0262f63368efd7caa68c9040
BranMM <78185047+BranMM@users.noreply.github.com>
2023-09-14 10:50:27 -0600

    Update assignment05.py

--- catkin_ws/src/students/scripts/assignment05.py
+++ catkin_ws/src/students/scripts/assignment05.py
@@ -18,7 +18,7 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 from navig_msgs.srv import SmoothPath, SmoothPathRequest
 from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
 
-NAME = "FULL NAME"
+NAME = "Brandon Meza Martínez"
 
 pub_goal_reached = None
 pub_cmd_vel = None
@@ -30,10 +30,16 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     
     #
     # TODO:
+    v_max = 0.5
+    w_max = 1
+    alpha = 1
+    beta = 1
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
+    error_a = (error_a + math.pi())%(2*math.pi())-math.pi()
     # Implement the control law given by:
     #
-    # v = v_max*math.exp(-error_a*error_a/alpha)
-    # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
+    v = v_max*math.exp(-error_a*error_a/alpha)
+    w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
     #
     # where error_a is the angle error and
     # v and w are the linear and angular speeds taken as input signals
@@ -42,6 +48,9 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # and return it (check online documentation for the Twist message).
     # Remember to keep error angle in the interval (-pi,pi]
     #
+    
+    cmd_vel.linear.x = v
+    cmd_vel.angular.z = w
     
     return cmd_vel
 
