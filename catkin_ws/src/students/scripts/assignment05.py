535857d33b8836d5360f326cdafb948f30ee180a
FelipeRDZF <36497391+FelipeRDZF@users.noreply.github.com>
2023-09-14 12:52:38 -0600

    Update assignment05.py

--- catkin_ws/src/students/scripts/assignment05.py
+++ catkin_ws/src/students/scripts/assignment05.py
@@ -18,12 +18,17 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 from navig_msgs.srv import SmoothPath, SmoothPathRequest
 from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
 
-NAME = "FULL NAME"
+NAME = "FELIPE RODRIGUEZ FUENTES"
 
 pub_goal_reached = None
 pub_cmd_vel = None
 loop        = None
 listener    = None
+
+v_max = 0.5 #Defino los valores maximos y las constantes del control de posicion
+w_max = 1.0
+alpha = 0.5
+betha = 0.5
 
 def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     cmd_vel = Twist()
@@ -31,9 +36,13 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     #
     # TODO:
     # Implement the control law given by:
-    #
-    # v = v_max*math.exp(-error_a*error_a/alpha)
-    # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
+    # Se define el error de angulo y su correccion para obtener velocidad lineal y angular
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
+    error_a = (error_a + math.pi) % (2*math.pi) - math.pi
+
+    
+    v = v_max*math.exp(-error_a*error_a/alpha)
+    w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
     #
     # where error_a is the angle error and
     # v and w are the linear and angular speeds taken as input signals
@@ -42,7 +51,8 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # and return it (check online documentation for the Twist message).
     # Remember to keep error angle in the interval (-pi,pi]
     #
-    
+    cmd_vel.linear.x = v
+    cmd_vel.angular.z = w
     return cmd_vel
 
 def follow_path(path):
