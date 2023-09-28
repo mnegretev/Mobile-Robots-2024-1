3eacf3a73902f8eef978bc1c3a4ae127566e4499
Students <you@example.com>
2023-09-28 12:34:08 -0500

    Tarea5

--- catkin_ws/src/students/scripts/assignment05.py
+++ catkin_ws/src/students/scripts/assignment05.py
@@ -40,8 +40,7 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # Implement the control law given by:
     #
     error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
-    if error_a < -math.pi or error_a > math.pi:
-    	error_a = (error_a + math.pi)%(2*math.pi) - math.pi
+    error_a = (error_a + math.pi)%(2*math.pi) - math.pi
     
     v = v_max*math.exp(-error_a*error_a/alpha)
     w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
