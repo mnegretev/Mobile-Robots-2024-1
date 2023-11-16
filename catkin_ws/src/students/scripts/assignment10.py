e1be31fae60afa9f88ee27bb4cb6bad27411f4cf
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-11-16 16:07:16 -0600

    Add files for final project

---
+++ catkin_ws/src/students/scripts/assignment10.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+# MOBILE ROBOTS - UNAM, FI, 2024-1
+# ASSIGNMENT 10 - SPEECH SYNTHESIS
+#
+
+import sys
+import rospy
+from sound_play.msg import SoundRequest
+
+def main(text_to_say):
+    print("INITIALIZING SPEECH SYNTHESIS TEST...")
+    rospy.init_node("speech_syn")
+    pub_speech = rospy.Publisher("/robotsound", SoundRequest, queue_size=10)
+    loop = rospy.Rate(2)
+
+    msg_speech = SoundRequest()
+    msg_speech.sound   = -3
+    msg_speech.command = 1
+    msg_speech.volume  = 1.0
+    #
+    # EJERCICIO
+    # Cambie la voz por alguna de las voces instaladas
+    #
+    msg_speech.arg2    = "voice_kal_diphone"
+    msg_speech.arg = text_to_say
+
+    loop.sleep()
+    print("Sending text to say: " + text_to_say)
+    pub_speech.publish(msg_speech)
+   
+if __name__ == "__main__":
+    text_to_say = "hello"
+    if len(sys.argv) > 1:
+        text_to_say = sys.argv[1]
+    main(text_to_say)
