c98e7bc24833e972881fdd990853b12a90656ba4
Diego Andres <diegoandresgarcia01@gmail.com>
2023-11-22 16:04:49 -0600

    tarea 07

--- catkin_ws/src/students/scripts/assignment09.py
+++ catkin_ws/src/students/scripts/assignment09.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "FULL_NAME"
+NAME = "Andres Garcia Diego"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
@@ -49,7 +49,11 @@ class NeuralNetwork(object):
         # Include input x as the first output.
         #
         y = []
-        
+        y.append(x)
+        for i in range(len(self.biases)):
+            z = numpy.dot(self.weights[i],x) + self.biases[i]
+            x = 1.0 / (1.0 + numpy.exp(-z))
+            y.append(x)
         return y
 
     def backpropagate(self, x, yt):
@@ -73,8 +77,16 @@ class NeuralNetwork(object):
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
         #
+        delta = (y[-1] - yt)*y[-1]*(1 - y[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = delta*y[-2].transpose()
+
+        for i in range(2, self.num_layers):
+            delta = numpy.dot(self.weights[-i + 1].transpose(), delta)*y[-i]*(1.0 - y[-i])
+            nabla_b[-i] = delta
+            nabla_w[-i] = numpy.dot(delta,y[-i - 1].transpose())
         
-        
+
         return nabla_w, nabla_b
 
     def update_with_batch(self, batch, eta):
@@ -160,7 +172,7 @@ def main():
         pass
     
     nn.train_by_SGD(training_dataset, epochs, batch_size, learning_rate)
-    #numpy.savez(dataset_folder + "network",w=nn.weights, b=nn.biases)
+    numpy.savez(dataset_folder + "network",w=nn.weights, b=nn.biases)
     
     print("\nPress key to test network or ESC to exit...")
     numpy.set_printoptions(formatter={'float_kind':"{:.3f}".format})
