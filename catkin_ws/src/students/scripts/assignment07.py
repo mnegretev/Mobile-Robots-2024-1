d6037134300b21926e316d27684c5d0007390a31
exu <toxam.metalmania@gmail.com>
2023-11-23 03:40:42 -0600

    Practica 4

--- catkin_ws/src/students/scripts/assignment07.py
+++ catkin_ws/src/students/scripts/assignment07.py
@@ -19,7 +19,7 @@ from geometry_msgs.msg import PointStamped, Point
 from vision_msgs.srv import RecognizeObject, RecognizeObjectResponse
 from vision_msgs.msg import VisionObject
 
-NAME = "FULL_NAME"
+NAME = "Camacho Sandoval Ricardo"
 
 def segment_by_color(img_bgr, points, obj_name):
     #
