7f36835844ee3a80e6b699cb30ccb8bc39581e24
Jerson Romero <30983514+jertword@users.noreply.github.com>
2023-11-23 16:02:03 -0600

    Update practice04.cpp

--- catkin_ws/src/students/src/practice04.cpp
+++ catkin_ws/src/students/src/practice04.cpp
@@ -21,7 +21,7 @@
 #include <numeric>
 
 
-#define FULL_NAME "Castro Serrato Luis Joaquin"
+#define FULL_NAME "Romero Trujillo Jerson Gerardo"
 
 #define LASER_DOWNSAMPLING  10
 #define SENSOR_NOISE        0.1
