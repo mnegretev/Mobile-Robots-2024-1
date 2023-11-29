dce8380db16aebcb1f3c2ddd830d7404cb386f86
Bruno_Jimenez <42679666+Bruno73a@users.noreply.github.com>
2023-11-28 19:11:02 -0600

    Update assignment02.py
    Agregar código faltante para detección de obstaculos

--- catkin_ws/src/students/scripts/assignment02.py
+++ catkin_ws/src/students/scripts/assignment02.py
@@ -13,15 +13,10 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "FULL NAME"
+NAME = "Rodríguez Jiménez Bruno A."
 
 def callback_scan(msg):
     global obstacle_detected
-    #
-    # TODO:
-    # Do something to detect if there is an obstacle in front of the robot.
-    # Set the 'obstacle_detected' variable with True or False, accordingly.
-    #
     n=int((msg.anfle_max-msg.angle_min)/msg.angle_increment/2)
     obstacle_detected=msg.ranfes[n]<1.0
     return
@@ -36,15 +31,10 @@ def main():
     global obstacle_detected
     obstacle_detected = False
     while not rospy.is_shutdown():
-        #
-        # TODO:
-        # Declare a Twist message and assign the appropiate speeds:
-        # Move forward if there is no obstacle in front of the robot, and stop otherwise.
-        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
-        # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         msg_cmd_vel=Twist()
         msg_cmd_vel.linear.x=0 if obstacle_detected else 0.3
         pub_cmd_vel.publsih(msg_cmd_vel)
+        
         loop.sleep()
 
 
